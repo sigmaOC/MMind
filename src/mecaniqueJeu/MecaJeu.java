7c4d92c5740dcb9eaec96b8075b66deea610f900
sigmaOC <stephanedartinet@orange.fr>
2022-11-19 14:42:48 +0100

    creation class MecaJeu

---
+++ src/mecaniqueJeu/MecaJeu.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mecaniqueJeu;
+
+/**
+ * @author steph
+ *
+ */
+public class MecaJeu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
